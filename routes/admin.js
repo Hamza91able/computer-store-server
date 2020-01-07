e64ff389ca895b9bb37e9ebbf102a0659afb22bf
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2020-01-08 02:39:43 +0500

    Added is-auth middle ware to secure apis with user authentication

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.post('/add-category', adminController.addCategory);
+router.post('/add-category', isAuth, adminController.addCategory);
 
 module.exports = router;
