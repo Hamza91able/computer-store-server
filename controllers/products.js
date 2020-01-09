ac9990059c56001ba53217362552cad4a6f883c4
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2020-01-10 04:01:46 +0500

    Added apis to filter data by brands and sub categories

--- controllers/products.js
+++ controllers/products.js
@@ -10,14 +10,22 @@ exports.getProductByCategory = (req, res, next) => {
     } else {
         sort = '-price'
     }
+    let totalDocuments;
 
     Products
         .find({ category: categoryName })
-        .sort(sort)
-        .then(products => {
-            res.status(200).json({
-                products: products,
-            });
+        .countDocuments()
+        .then(totalDocuments => {
+            totalDocuments = totalDocuments;
+            return Products
+                .find({ category: categoryName })
+                .sort(sort)
+                .then(products => {
+                    res.status(200).json({
+                        products: products,
+                        totalDocuments: totalDocuments
+                    });
+                })
         })
         .catch(err => {
             console.log(err);
@@ -40,6 +48,74 @@ exports.getSpecificProduct = (req, res, next) => {
         .catch(err => {
             res.status(500).json({
                 message: 'Internal Server Error',
+            });
+        });
+}
+
+exports.getProductsBySubCategory = (req, res, next) => {
+    const subCategoryName = req.params.name;
+    const order = req.params.order;
+
+    let sort = 'price'
+    if (order === '10') {
+        sort = 'price'
+    } else {
+        sort = '-price'
+    }
+
+    Products
+        .find({ subCategory: subCategoryName })
+        .countDocuments()
+        .then(totalDocuments => {
+            totalDocuments = totalDocuments;
+            return Products
+                .find({ subCategory: subCategoryName })
+                .sort(sort)
+                .then(products => {
+                    res.status(200).json({
+                        products: products,
+                        totalDocuments: totalDocuments
+                    });
+                })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "Internal Server Error",
+            });
+        });
+}
+
+exports.getProductsByBrands = (req, res, next) => {
+    const brand = req.params.name;
+    const order = req.params.order;
+
+    let sort = 'price'
+    if (order === '10') {
+        sort = 'price'
+    } else {
+        sort = '-price'
+    }
+
+    Products
+        .find({ brand: brand })
+        .countDocuments()
+        .then(totalDocuments => {
+            totalDocuments = totalDocuments;
+            return Products
+                .find({ brand: brand })
+                .sort(sort)
+                .then(products => {
+                    res.status(200).json({
+                        products: products,
+                        totalDocuments: totalDocuments
+                    });
+                })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "Internal Server Error",
             });
         });
 }
--- routes/products.js
+++ routes/products.js
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.get('/get-products-by-category/:categoryName/:order', productController.getProductByCategory);
 router.get('/get-specific-product/:id', productController.getSpecificProduct);
+router.get('/get-products-by-sub-category/:name/:order', productController.getProductsBySubCategory);
+router.get('/get-products-by-brand/:name/:order', productController.getProductsByBrands);
 
 module.exports = router;
